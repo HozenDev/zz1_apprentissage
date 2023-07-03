78bb41eaa3b43c0c10cd44da0da3fddde50508cc
loleroy3 <loleroy3@ada.local.isima.fr>
2023-07-03 10:29:44 +0200

    Structure random change for recuit

--- src/resolution/resolution.c
+++ src/resolution/resolution.c
@@ -1,3 +1,5 @@
+#include "../seed/seed.h"
+
 float resolution_recuis_simule(float (*pf)(float),regle_t ** solution)
 {
     /* paramètres */
@@ -7,6 +9,7 @@ float resolution_recuis_simule(float (*pf)(float),regle_t ** solution)
           espsilon = 0.001;
     int indicerand=0;
     int indicerand2=0;
+    generate_seed(0);
 
 
     /* allocation des tableaux de solutions et solutions + 1 */
@@ -45,3 +48,28 @@ float resolution_recuis_simule(float (*pf)(float),regle_t ** solution)
     free(new);
     return distmin;
 }   
+
+
+void resolution_random_change(regle_t ** solution)
+{
+    //selecting modified rules
+    int random_rule=rand()%10;
+    //selecting modified attributes
+    int random_attribute=rand()%5;
+    
+    
+    //apllying modification
+    switch (random_attribute)
+    {
+    case 1:
+        // to do replace 1st value within measures
+    case 2:
+        // to do replace 2nd value within measures
+    case 3: 
+        //to  do replace 3rd value within measures
+    case 4:
+        //to do replace value of action
+    case 5:
+        //to do replace value of priority
+    }
+}
