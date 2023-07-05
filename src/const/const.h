a01fbdd7925031a2ef38bc5262751dc4c879955a
Hozen <enzo.durel@gmail.com>
2023-07-05 14:03:28 +0200

    pour recup

--- src/const/const.h
+++ src/const/const.h
@@ -38,7 +38,7 @@
 #define NB_DISTANCE    3
 #define NB_CARDINALITY 6
 #define NB_MEASURE     4   // size of measures
-#define NB_RULES       13  // number rules in brain (size of array_rules)
+#define NB_RULES       14  // number rules in brain (size of array_rules)
 
 
 /*RECUIS*/
